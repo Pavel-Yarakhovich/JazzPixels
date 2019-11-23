b4608bc10b7e228b2709b6addbbf9bd48392e826
Pavel-Yarakhovich <pavel.yarri@gmail.com>
2019-11-23 19:53:56 +0300

    pixel perfect is checked, logic is improved

--- app/js/map.js
+++ app/js/map.js
@@ -1,28 +1,67 @@
 const eventsButton = document.querySelector('.content__events');
 const locationsButton = document.querySelector('.content__locations');
 const footerLinks = document.querySelectorAll('.footer__map_link');
+const eventsLink = document.querySelector('#events_link');
+const linkNumberOfNewEvents = document.querySelector('#booked-events');
+const buttonNumberOfNewEvents = document.querySelector('#button_booked-events');
+const pageHeaders = document.querySelectorAll('.page-header');
+let currentLink = '';
+const articles = document.querySelectorAll('.content__article');
+let newEvents;
 
-const expandEvents = () => {
+document.addEventListener("DOMContentLoaded", () => {
+	articles.forEach(art => art.classList.add('event-new'));
+	newEvents = articles.length;
+	linkNumberOfNewEvents.textContent = newEvents;
+	buttonNumberOfNewEvents.textContent = newEvents;
+});
+
+const expandNewEvents = () => {
 	articles.forEach((art, i) => {
 		setTimeout(() => {
-			art.classList.toggle('content__article-hidden')
+			art.classList.remove('content__article-hidden')
 		}, (i + 1)* 150);
 	})
 	eventsButton.classList.add('hidden');
 	locationsButton.classList.add('hidden');
+	pageHeaders[0].classList.remove('page-header-hidden');
 }
 
-eventsButton.addEventListener('click', expandEvents);
+const showHeader = () => {
+		!currentLink === 'events_link'
+			? pageHeaders[0].classList.remove('page-header-hidden')
+			: null
+}
 
 const togglePages = (e) => {
-	event.preventDefault(e);
-	articles.forEach(art => {
-		art.classList.remove('booked-displayed');
-		art.classList.contains('booked')
-			? art.classList.add('booked-displayed') : null;
-	})
-};
+	!e.target.id === 'feed_link' 
+		? e.preventDefault() : null ;
+	currentLink = e.target.id;
+	
+	showHeader();
+}
+
+
+articles.forEach(art => art.addEventListener('click', () => {
+	if (art.classList.contains('event-new')) {
+		art.classList.remove('event-new');
+		art.classList.add('booked-displayed');
+		setTimeout(()=> {
+			art.classList.remove('booked-displayed');
+			art.classList.add('booked')
+		}, 1000);
+		newEvents--;
+		linkNumberOfNewEvents.textContent = newEvents;
+		buttonNumberOfNewEvents.textContent = newEvents;
+	}
+}));
+
+eventsButton.addEventListener('click', expandNewEvents);
+eventsLink.addEventListener('click', expandNewEvents);
+
 
 footerLinks.forEach(link => 
 	link.addEventListener('click', togglePages)
 );
+
+
